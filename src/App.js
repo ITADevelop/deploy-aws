b73a2967d625853e225038222cb31d7ad7de2511
Omar Soto <omar.soga97dev@outlook.com>
2022-01-31 17:26:45 -0600

    Changes for V2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Desplegar  <code> Aplicación</code> 
         </p>
         <a
           className="App-link"
